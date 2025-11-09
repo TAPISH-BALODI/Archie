0120a18940ac8eb846dcaf13ad4ffb203e60d6e2
Tapish-Evivve <69200543+TAPISH-BALODI@users.noreply.github.com>
2025-11-09 20:40:13 +0530

    build and env setttings

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,4 +1,4 @@
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { useAppState, getProjectStatus } from '../store';
 import { ProgressBar } from '../components/ProgressBar';
 import { StatusBadge } from '../components/StatusBadge';
